0529e2373a66e8bcb55c35372fbc9c7dae3ce3a6
Browdy11 <daffa.abar200@gmail.com>
2026-03-25 23:57:49 +0700

    p
    Added main method to SchoolApp for student management and score display.

--- Modul2/Modul2Pemlan enkapsulasi/SchoolApp.java
+++ Modul2/Modul2Pemlan enkapsulasi/SchoolApp.java
@@ -1,3 +1,16 @@
 public class SchoolApp {
+    public static void main(String[] args) {
+        Student miki = new Student("Miki");
+        Student mini = new Student("Mini");
 
+        // Pemanggilan variabel diubah menggunakan method getter
+        System.out.println("Grade " + miki.getNama() + " " + miki.getSkor());
+        System.out.println("Grade " + mini.getNama() + " " + mini.getSkor());
+
+        // Mengubah nama menggunakan method public static sesuai instruksi langkah 5b
+        Student.setNama(mini, "Mono");
+
+        System.out.println("Grade " + miki.getNama() + " " + miki.getSkor());
+        System.out.println("Grade " + mini.getNama() + " " + mini.getSkor());
+    }
 }
